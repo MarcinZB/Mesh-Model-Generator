827d3e31a698dc82b951c1c0916e7796aaba8f32
Marcin Zbroszczyk <marcin.zbroszczyk@outlook.com>
2022-12-13 13:50:16 +0100

    new_module

--- cloud_box.py
+++ cloud_box.py
@@ -5,7 +5,9 @@ import tkinter
 import os
 from tkinter import filedialog
 from pathlib import Path
+import math
 
+points = []
 
 filename = filedialog.askopenfilename(initialdir="/",
                                           title="Select a File",
@@ -25,4 +27,43 @@ point_cloud = np.loadtxt(all_name, skiprows=1)
 pcd = od.geometry.PointCloud()
 pcd.points = od.utility.Vector3dVector(point_cloud[:, :3])
 pcd.colors = od.utility.Vector3dVector(point_cloud[:, 3:6] / 255)
-od.visualization.draw_geometries([pcd])
+#od.visualization.draw_geometries([pcd])
+
+absolute_path = f"{input_path}"
+csv_wo_normals = pd.read_csv(absolute_path, sep=" ")
+print(f"Csv without normals: \n {csv_wo_normals}")
+
+
+frame = pd.DataFrame(csv_wo_normals)
+
+maximum_value_x = csv_wo_normals["X" or "x"].max()
+maximum_value_y = csv_wo_normals["Y" or "y"].max()
+maximum_value_z = csv_wo_normals["Z" or "z"].max()
+
+minimum_value_x = csv_wo_normals["X" or "x"].min()
+minimum_value_y = csv_wo_normals["Y" or "y"].min()
+minimum_value_z = csv_wo_normals["Z" or "z"].min()
+
+print(f"Maximum x value: {maximum_value_x}")
+print(f"Maximum y value: {maximum_value_y}")
+print(f"Maximum z value: {maximum_value_z}")
+
+print(f"Minimum x value: {minimum_value_x}")
+print(f"Minimum y value: {minimum_value_y}")
+print(f"Minimum z value: {minimum_value_z}")
+
+list_of_value = [minimum_value_x,minimum_value_y,minimum_value_z,maximum_value_x,
+maximum_value_y, maximum_value_z]
+
+print(list_of_value)
+
+minimum_value = min(list_of_value)
+max_value = max(list_of_value)
+
+line_length = ((math.fabs(minimum_value)+2)+(math.fabs(max_value)+2))
+print(line_length)
+
+print(f"X value: ({minimum_value_x};{maximum_value_x})")
+print(f"Y value: ({minimum_value_y};{maximum_value_y})")
+print(f"Z value: ({minimum_value_z};{maximum_value_z})")
+
